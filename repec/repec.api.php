f6d12760de108ef48fd3d1534e279dfbca6db123
Everson <dasilva@urbaninsight.com>
2015-04-02 14:24:07 -0700

    Add RePEc.

---
+++ repec/repec.api.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * RePEc module hooks.
+ */
+
+/**
+ * Implements hook_repec_paper_mapping().
+ */
+function hook_repec_paper_mapping($node) {
+}
+
+/**
+ * Implements hook_repec_paper_mapping_alter().
+ */
+function hook_repec_paper_mapping_alter(&$data) {
+}
+
+/**
+ * Implements hook_repec_series_mapping().
+ */
+function hook_repec_series_mapping() {
+}
+
+/**
+ * Implements hook_repec_series_mapping_alter().
+ */
+function hook_repec_series_mapping_alter(&$data) {
+}
+
+/**
+ * Implements hook_repec_archive_mapping().
+ */
+function repec_repec_archive_mapping() {
+}
+
+/**
+ * Implements hook_repec_archive_mapping_alter().
+ */
+function repec_repec_archive_mapping_alter(&$data) {
+}
+
+
